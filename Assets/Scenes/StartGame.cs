59e528aae22e35d28d6d55bcfabc45d5a3e9530a
LukaaBazooka <113695215+LukaaBazooka@users.noreply.github.com>
2023-05-23 16:13:26 +0100

    menu
    menu

---
+++ Assets/Scenes/StartGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{   
+    public RawImage images;
+
+    public void Runs()
+    {
+        StartCoroutine(PlayGame());
+    }
+    public IEnumerator PlayGame()
+    {
+        LeanTween.alpha(images.rectTransform, 1f, 2f);
+
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
